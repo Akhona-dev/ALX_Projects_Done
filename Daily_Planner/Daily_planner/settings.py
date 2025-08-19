3890725d378eea4691155248f216b35666c5bfc2
Akhona Trom <akhonatrom1@gmail.com>
2025-08-19 14:40:34 +0200

    add changes

--- Daily_Planner/Daily_planner/settings.py
+++ Daily_Planner/Daily_planner/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'task_management',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'Daily_planner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DAILY',
+        'USER': 'root',
+        'PASSWORD': 'akhona',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- Daily_Planner/task_management/models.py
+++ Daily_Planner/task_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tasks(models.Model):
+    morning = models.TextField()
+    afternoon = models.TextField()
+    evening = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='author_name'
+    )
